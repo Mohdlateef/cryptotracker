72d8fe5a7f7072e8796f3dd2f7b9773fc7753b6d
Mohdlateef <lateefahbaba123@gmail.com>
2024-04-26 22:01:42 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Common/Header';
+import Footer from './components/Common/Footer';
+function App() {
+  return (
+    <div className="App">
+   <Header/>
+gfdf
+   <h1>hi start  </h1>
+   <Footer/>
+    </div>
+  );
+}
+
+export default App;
